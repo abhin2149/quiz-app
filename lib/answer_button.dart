e3a99897d274c04fef72afab112a4f75d8bf7098
Abhinav <abhin2149@gmail.com>
2024-06-14 09:52:08 +0530

    Update questions screen

---
+++ lib/answer_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.label, this.onClick, {super.key});
+
+  final String label;
+  final void Function() onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onClick,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 40,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(80),
+          ),
+        ),
+        child: Text(
+          label,
+        ));
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/styled_text.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,8 +14,18 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Hello from questions"),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const StyledText('Question', 15),
+          const SizedBox(height: 50),
+          AnswerButton('Option 1', () => {}),
+          AnswerButton('Option 2', () => {}),
+          AnswerButton('Option 3', () => {}),
+          AnswerButton('Option 4',() => {}),
+        ],
+      )
     );
   }
 }
